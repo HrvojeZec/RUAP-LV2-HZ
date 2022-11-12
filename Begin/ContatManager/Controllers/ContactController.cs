9f89cd32134fec724ebedad73dd4534163a14154
Hrvoje Zec <hrvoje.zec99@gmail.com>
2022-11-12 21:17:59 +0100

    zadatak3

--- Begin/ContatManager/Controllers/ContactController.cs
+++ Begin/ContatManager/Controllers/ContactController.cs
@@ -21,6 +21,14 @@ namespace ContatManager.Controllers
         {
             return this.contactRepository.GetAllContacts();
         }
-           
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
     }
 }
